b94b1bb82493b00f75db82386233dc2082fbe895
Aaronanderson01 <aaronanderson01@gmail.com>
2016-10-08 10:48:40 -0600

    initial commit

---
+++ js/mainCtrl.js
@@ -0,0 +1,9 @@
+// INITILIZE CONTROLLER
+// ============================================================
+angular.module("timeApp").controller("mainCtrl", function($scope) {
+
+  $scope.name = 'Aaron';
+
+  // FUNCTIONS
+  // ============================================================
+});
